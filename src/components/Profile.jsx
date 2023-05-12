e2c5f46e64e478d09572645be56cf48965d7db65
Shilpa Sharma <shilpasharma118@gmail.com>
2023-05-11 22:23:35 -0700

    adding component Will Unmount in useEffect()

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Profile = ({ name }) => {
   const [count, setCount] = useState(0);
   const [count2, setCount2] = useState(1);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("React Course----");
+    }, 1000);
+
+    //This function is called when you unmount your component
+    return () => {
+      clearInterval(timer);
+      console.log("Here you can call anything which has to be cleaned up");
+    };
+  });
   return (
     <div>
       <h1>Profile Functional Component : {name}</h1>
